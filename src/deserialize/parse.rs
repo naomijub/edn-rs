3ed809886c6a70683cc45119a88bda0d3ad43cbb
Julia Naomi <jnboeira@outlook.com>
2020-09-12 14:03:20 -0300

    use debug mode when char is not parsed

--- src/deserialize/parse.rs
+++ src/deserialize/parse.rs
@@ -24,7 +24,7 @@ pub(crate) fn parse_edn(c: Option<char>, chars: &mut std::str::Chars) -> Result<
         Some(b) if b == 't' || b == 'f' || b == 'n' => Ok(read_bool_or_nil(b, chars)?),
         Some(n) if n.is_numeric() => Ok(read_number(n, chars)?),
         a => Err(Error::ParseEdn(format!(
-            "{} could not be parsed",
+            "{:?} could not be parsed",
             a.unwrap_or(' ').to_string()
         ))),
     }
