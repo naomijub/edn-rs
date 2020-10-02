18ac48d1b83fdaef5cbd10581bbb57f809138a47
Julia Naomi <jnboeira@outlook.com>
2020-10-02 13:33:47 -0300

    string types

--- src/json/mod.rs
+++ src/json/mod.rs
@@ -12,15 +12,15 @@ pub(crate) fn display_as_json(edn: &Edn) -> String {
         Edn::List(_) => unimplemented!(),
         Edn::Key(_) => unimplemented!(),
         Edn::Symbol(_) => unimplemented!(),
-        Edn::Str(_) => unimplemented!(),
+        Edn::Str(s) => format!("{:?}", s),
         Edn::Int(n) => format!("{}", n),
         Edn::UInt(n) => format!("{}", n),
         Edn::Double(n) => format!("{}", n),
         Edn::Rational(r) => format!("{}", rational_to_double(r).unwrap()),
         Edn::Char(c) => format!("'{}'", c),
         Edn::Bool(b) => format!("{}", b),
-        Edn::Inst(_) => unimplemented!(),
-        Edn::Uuid(_) => unimplemented!(),
+        Edn::Inst(inst) => format!("{:?}", inst),
+        Edn::Uuid(uuid) => format!("{:?}", uuid),
         Edn::NamespacedMap(_, _) => unimplemented!(),
         Edn::Nil => String::from("null"),
         Edn::Empty => String::from(""),
@@ -70,5 +70,26 @@ mod test {
     fn chars() {
         assert_eq!(display_as_json(&Edn::Char('e')), String::from("'e'"));
         assert_eq!(display_as_json(&Edn::Char('5')), String::from("'5'"));
+    }
+
+    #[test]
+    fn inst_and_uuid() {
+        let inst = Edn::Inst("2020-09-18T01:16:25.909-00:00".to_string());
+        let uuid = Edn::Uuid("af6d8699-f442-4dfd-8b26-37d80543186b".to_string());
+
+        assert_eq!(
+            display_as_json(&inst),
+            "\"2020-09-18T01:16:25.909-00:00\"".to_string()
+        );
+        assert_eq!(
+            display_as_json(&uuid),
+            "\"af6d8699-f442-4dfd-8b26-37d80543186b\"".to_string()
+        );
+    }
+
+    #[test]
+    fn strings() {
+        let edn = Edn::Str("Hello World".to_string());
+        assert_eq!(display_as_json(&edn), "\"Hello World\"".to_string());
     }
 }
