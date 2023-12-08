1655d364994c6b14d2dd12faf08b8a6df3d4f85e
Kevin Nakamura <grinkers@grinkers.net>
2023-12-08 09:11:13 +0900

    Refactor deserialization integration tests to `tests/` using `from_str`, avoiding private functions. Nicely organize `sets` feature tests in `tests/lib.rs`

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,5 +1,11 @@
 #![recursion_limit = "512"]
 
+pub mod deserialize;
 pub mod emit;
 pub mod parse;
 pub mod ser;
+
+#[cfg(feature = "sets")]
+pub mod deserialize_sets;
+#[cfg(feature = "sets")]
+pub mod parse_sets;
